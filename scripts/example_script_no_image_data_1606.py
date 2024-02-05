d41b2a575d4986f43d1d2b849c0229062228adb7
Ryan Keeley <rkeeley@rclogin02.cluster>
2024-02-05 12:16:22 -0800

    updated 1606 script with sat position

--- scripts/example_script_no_image_data_1606.py
+++ scripts/example_script_no_image_data_1606.py
@@ -16,8 +16,8 @@ kwargs_sample_realization = {'log10_sigma_sub': ['FIXED',np.log10(0.05)],
 kwargs_sample_source = {'source_size_pc': ['UNIFORM', 1, 10]}
 kwargs_sample_macro_fixed = {
     'satellite_1_theta_E': ['GAUSSIAN', 0.2, 0.025],
-    'satellite_1_x': ['GAUSSIAN', 0.5, 0.025],
-    'satellite_1_y': ['GAUSSIAN', 0.75, 0.025],
+    'satellite_1_x': ['GAUSSIAN', -0.25888077, 0.025],
+    'satellite_1_y': ['GAUSSIAN', -1.1975569474999999, 0.025],
     # 'a4_a': ['FIXED', data_class.a4a_true],
     # 'a3_a': ['FIXED', data_class.a3a_true],
      #'delta_phi_m3': ['FIXED', data_class.delta_phi_m3_true],
