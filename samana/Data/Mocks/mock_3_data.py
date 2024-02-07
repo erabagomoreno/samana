bd23926b81b4bbcce636b25c6a6614923d66c750
Daniel Gilman <gilmanda@ucla.edu>
2024-02-07 00:26:25 -0600

    add psf3 lenses

--- samana/Data/Mocks/mock_3_data.py
+++ samana/Data/Mocks/mock_3_data.py
@@ -3,11 +3,11 @@ from samana.Data.Mocks.base import MockBase
 from samana.Data.ImageData.mock_3_simple import image_data as simple_image_data
 from samana.Data.ImageData.mock_3_cosmos import image_data as cosmos_image_data
 from samana.Data.ImageData.mock_3_2038 import image_data as simulated_2038_image_data
-
+from samana.Data.ImageData.mock_3_cosmos_psf3 import image_data as cosmos_image_data_psf3
 
 class Mock3Data(MockBase):
 
-    def __init__(self, super_sample_factor=1.0, cosmos_source=False, sim2038_source=False):
+    def __init__(self, super_sample_factor=1.0, cosmos_source=False, sim2038_source=False, cosmos_source_psf3=False):
 
         z_lens = 0.5
         z_source = 2.5
@@ -27,8 +27,26 @@ class Mock3Data(MockBase):
             image_data = cosmos_image_data
         elif sim2038_source:
             image_data = simulated_2038_image_data
+        elif cosmos_source_psf3:
+            image_data = cosmos_image_data_psf3
         else:
             image_data = simple_image_data
         super(Mock3Data, self).__init__(z_lens, z_source, x_image, y_image,
                                     magnifications, astrometric_uncertainties, flux_ratio_uncertainties, image_data,
                                         super_sample_factor)
+
+
+class Mock3DataPSF3(Mock3Data):
+
+    def __init__(self, super_sample_factor=1.0):
+        super(Mock3DataPSF3, self).__init__(super_sample_factor, cosmos_source_psf3=True)
+
+    @property
+    def kwargs_psf(self):
+        fwhm = 0.3
+        deltaPix = self.coordinate_properties[0]
+        kwargs_psf = {'psf_type': 'GAUSSIAN',
+                      'fwhm': fwhm,
+                      'pixel_size': deltaPix,
+                      'truncation': 5}
+        return kwargs_psf
